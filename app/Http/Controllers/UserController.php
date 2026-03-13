774ea8bf8374df8c49a12feab4e551746bcc94cf
fazelazel <fazelpriyono@gmail.com>
2026-03-13 15:07:54 +0700

    jobsheet 4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,14 +9,52 @@ class UserController extends Controller
 {
     public function index()
     {
-        // tambah data user dengan Eloquent Model
-        $data = [
-            'nama' => 'Pelanggan Pertama',
-        ];
-        UserModel::where('username', 'customer-1') ->update($data); // update data user
-    
-        // coba akses model user model
-        $user = UserModel::all(); // Mengambil semua data dari tabel m_user menggunakan model UserModel
-        return view('user', ['data' => $user]); // Mengirim data ke view
+        $user = UserModel::with('level')->get();
+        return view('user', ['data' => $user]);
+    }
+
+    public function tambah()
+    {
+        return view('user_tambah');
+    }
+
+    public function tambah_simpan(Request $request)
+    {
+        UserModel::create([
+            'username' => $request->username,
+            'nama' => $request->nama,
+            'password' => Hash::make('$request->password'),
+            'level_id' => $request->level_id,
+        ]);
+
+        return redirect('/user');
+    }
+
+    public function ubah($id)
+    {
+        $user = UserModel::find($id);
+        return view('user_ubah', ['data' => $user]);
+    }
+
+    public function ubah_simpan(Request $request, $id)
+    {
+        $user = UserModel::find($id);
+        $user->username = $request->username;
+        $user->nama = $request->nama;
+        if ($request->password) {
+            $user->password = Hash::make($request->password);
+        }
+        $user->level_id = $request->level_id;
+        $user->save();
+
+        return redirect('/user');
+    }
+
+    public function hapus($id)
+    {
+        $user = UserModel::find($id);
+        $user->delete();
+
+        return redirect('/user');
     }
 }
